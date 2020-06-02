1089d0fcc88bd15b2ea68be9fcfcd62fdcba1bf8
azocher <anna.zocher@gmail.com>
2020-06-02 08:17:14 -0700

    added reduce solution

--- reduce.js
+++ reduce.js
@@ -2,9 +2,10 @@
 
 var numbers = [1, 56, 2, 4, 1, 99, 3, 5];
 
-// Your code here!
+// adding reducer method
+var reducer = function(accumulator, currentValue) {
+    return accumulator * currentValue;
+}
 
-// Write something that takes the above array and returns the product of all the numbers
-// via multiplication
-
+console.log(numbers.reduce(reducer));
 
